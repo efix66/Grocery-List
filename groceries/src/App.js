ba42fc74fe4a5a08e527834859f107ee3a0ba7e4
Andries Felix <56032612+efix66@users.noreply.github.com>
2022-07-25 22:05:30 +0300

    Small update

--- groceries/src/App.js
+++ groceries/src/App.js
@@ -5,11 +5,9 @@ import Footer from "./components/Footer";
 import Content from "./components/Content";
 
 function App() {
-	const [items, setItems] = useState([
-		{ id: 1, checked: false, item: "One half pound of Cocoa" },
-		{ id: 2, checked: false, item: "Item 2" },
-		{ id: 3, checked: false, item: "Item 3" },
-	]);
+	const [items, setItems] = useState(
+		JSON.parse(localStorage.getItem("shoppinglist"))
+	);
 
 	const [newItem, setNewItem] = useState("");
 
